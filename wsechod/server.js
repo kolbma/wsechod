0b32f53365ee42052bcf02885989d600ad82f0b3
Markus Kolb <github@tower-net.de>
2019-04-05 01:40:38 +0200

    fixed tslint warnings

--- wsechod/server.js
+++ wsechod/server.js
@@ -1,18 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-const http = require("http");
-const https = require("https");
-const ws = require("ws");
-class Server {
-    constructor() {
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var ws = require("ws");
+var Server = /** @class */ (function () {
+    function Server() {
         this.heartbeatInterval = Server.DEFAULT_HEARTBEAT_INTERVAL;
     }
-    initServer(listeningPort, ishttps = false, heartbeatInterval) {
-        this.heartbeatInterval = heartbeatInterval | this.heartbeatInterval;
-        let server;
+    // constructor() { }
+    Server.prototype.initServer = function (listeningPort, ishttps, heartbeatInterval) {
+        if (ishttps === void 0) { ishttps = false; }
+        this.heartbeatInterval = heartbeatInterval || this.heartbeatInterval;
+        var server;
         if (ishttps) {
-            let options = { cert: fs.readFileSync('cert.pem'), key: fs.readFileSync('key.pem'), requestCert: false, rejectUnauthorized: false };
+            var options = { cert: fs.readFileSync('cert.pem'), key: fs.readFileSync('key.pem'), requestCert: false, rejectUnauthorized: false };
             if (fs.existsSync('ca.pem')) {
                 options.ca = fs.readFileSync('ca.pem');
             }
@@ -21,7 +23,7 @@ class Server {
             server.maxHeadersCount = 20;
             server.timeout = 5000;
             server.keepAliveTimeout = 5000;
-            server.listen(listeningPort, "localhost", 20, () => {
+            server.listen(listeningPort, 'localhost', 20, function () {
                 console.log('listening on %s://localhost:%d', (ishttps ? 'wss' : 'ws'), listeningPort);
             });
         }
@@ -31,33 +33,32 @@ class Server {
             server.timeout = 5000;
             server.maxConnections = 20;
             server.maxHeadersCount = 20;
-            server.listen(listeningPort, "localhost", 20, () => {
+            server.listen(listeningPort, 'localhost', 20, function () {
                 console.log('listening on %s://localhost:%d', (ishttps ? 'wss' : 'ws'), listeningPort);
             });
         }
-        const wss = new ws.Server({ server: server, maxPayload: 50, backlog: 20 });
+        var wss = new ws.Server({ server: server, maxPayload: 50, backlog: 20 });
         this.initHeartbeat(wss);
-        wss.on('connection', (websocket, req) => {
+        wss.on('connection', function (websocket, req) {
             websocket.isAlive = true;
-            let address = req.socket.remoteAddress;
-            let port = req.socket.remotePort;
-            let ip = req.headers['x-forwarded-for'];
+            var address = req.socket.remoteAddress;
+            var port = req.socket.remotePort;
+            var ip = req.headers['x-forwarded-for'];
             if (ip && ip.length > 0) {
                 ip = ip.slice(0, ip.indexOf(','));
                 address = ip.slice(0, ip.indexOf(':')).toString();
-                port = Number.parseInt(ip.slice(ip.indexOf(':')).toString());
+                port = Number.parseInt(ip.slice(ip.indexOf(':')).toString(), 10);
             }
-            let timestamp = new Date().toISOString();
-            console.log('[%s] [%s:%s] connected', timestamp, address, port);
-            websocket.on('open', () => {
-                let timestamp = new Date().toISOString();
+            console.log('[%s] [%s:%s] connected', new Date().toISOString(), address, port);
+            websocket.on('open', function () {
+                var timestamp = new Date().toISOString();
                 console.log('[%s] [%s:%s] opened', timestamp, address, port);
             });
-            websocket.on('message', (message) => {
-                let timestamp = new Date().toISOString();
+            websocket.on('message', function (message) {
+                var timestamp = new Date().toISOString();
                 console.log('[%s] [%s:%s] receiving', timestamp, address, port);
-                let len = 0;
-                let text = '';
+                var len = 0;
+                var text = '';
                 if (typeof message === 'string') {
                     len = message.length;
                     text = 'chars string';
@@ -72,42 +73,44 @@ class Server {
                 timestamp = new Date().toISOString();
                 console.log('[%s] [%s:%s] sent %d %s', timestamp, address, port, len, text);
             });
-            websocket.on('close', (code, message) => {
-                let timestamp = new Date().toISOString();
+            websocket.on('close', function (code, message) {
+                var timestamp = new Date().toISOString();
                 console.log('[%s] [%s:%s] closed %s (%d)', timestamp, address, port, message, code);
             });
-            websocket.on('error', (err) => {
-                let timestamp = new Date().toISOString();
+            websocket.on('error', function (err) {
+                var timestamp = new Date().toISOString();
                 console.log('[%s] [%s:%s] error %s: %s: %s', timestamp, address, port, err.name, err.message, err.stack);
             });
-            websocket.on('pong', (data) => {
+            websocket.on('pong', function (data) {
                 websocket.isAlive = true;
             });
         });
-        wss.on('error', (err) => {
-            let timestamp = new Date().toISOString();
+        wss.on('error', function (err) {
+            var timestamp = new Date().toISOString();
             console.log('[%s] error %s: %s: %s', timestamp, err.name, err.message, err.stack);
         });
         return wss;
-    }
-    initHeartbeat(wsserver) {
-        setInterval(() => {
-            wsserver.clients.forEach((ws) => {
-                var x = ws;
-                if (ws.isAlive === false) {
-                    let timestamp = new Date().toISOString();
+    };
+    Server.prototype.initHeartbeat = function (wsserver) {
+        setInterval(function () {
+            wsserver.clients.forEach(function (wss) {
+                if (wss.isAlive === false) {
+                    var timestamp = new Date().toISOString();
                     console.log('[%s] heartbeat terminate', timestamp);
-                    return ws.terminate();
+                    return wss.terminate();
                 }
-                ;
-                ws.isAlive = false;
-                ws.ping('', false, true);
-                //log(LOGTYPE.DEBUG, 'ping');
+                wss.isAlive = false;
+                wss.ping('', false, function (err) {
+                    var timestamp = new Date().toISOString();
+                    console.log('[%s] error %s: %s: %s', timestamp, err.name, err.message, err.stack);
+                });
+                // log(LOGTYPE.DEBUG, 'ping');
             });
         }, this.heartbeatInterval);
-    }
-}
-Server.DEFAULT_HEARTBEAT_INTERVAL = 4000;
-Server.DEFAULT_PORT = 8080;
+    };
+    Server.DEFAULT_HEARTBEAT_INTERVAL = 4000;
+    Server.DEFAULT_PORT = 8080;
+    return Server;
+}());
 exports.Server = Server;
 //# sourceMappingURL=server.js.map
--- wsechod/server.ts
+++ wsechod/server.ts
@@ -3,120 +3,120 @@ import * as http from 'http';
 import * as https from 'https';
 import * as ws from 'ws';
 
-
 export class Server {
-    static readonly DEFAULT_HEARTBEAT_INTERVAL = 4000;
-    static readonly DEFAULT_PORT = 8080;
+  static readonly DEFAULT_HEARTBEAT_INTERVAL = 4000;
+  static readonly DEFAULT_PORT = 8080;
 
-    private heartbeatInterval: number = Server.DEFAULT_HEARTBEAT_INTERVAL;
+  private heartbeatInterval: number = Server.DEFAULT_HEARTBEAT_INTERVAL;
 
-    constructor() { }
+  // constructor() { }
 
-    initServer(listeningPort: number, ishttps: boolean = false, heartbeatInterval?: number): ws.Server {
-        this.heartbeatInterval = heartbeatInterval | this.heartbeatInterval;
+  initServer(listeningPort: number, ishttps: boolean = false, heartbeatInterval?: number): ws.Server {
+    this.heartbeatInterval = heartbeatInterval || this.heartbeatInterval;
 
-        let server: http.Server | https.Server;
-        if (ishttps) {
-            let options = { cert: fs.readFileSync('cert.pem'), key: fs.readFileSync('key.pem'), requestCert: false, rejectUnauthorized: false };
-            if (fs.existsSync('ca.pem')) {
-                (<any>options).ca = fs.readFileSync('ca.pem');
-            }
-            server = https.createServer(options);
-            server.maxConnections = 20;
-            (<any>server).maxHeadersCount = 20;
-            (<any>server).timeout = 5000;
-            (<any>server).keepAliveTimeout = 5000;
-            server.listen(listeningPort, "localhost", 20, () => {
-                console.log('listening on %s://localhost:%d', (ishttps ? 'wss' : 'ws'), listeningPort);
-            });
+    let server: http.Server | https.Server;
+    if (ishttps) {
+      const options = { cert: fs.readFileSync('cert.pem'), key: fs.readFileSync('key.pem'), requestCert: false, rejectUnauthorized: false };
+      if (fs.existsSync('ca.pem')) {
+        (options as any).ca = fs.readFileSync('ca.pem');
+      }
+      server = https.createServer(options);
+      server.maxConnections = 20;
+      (server as any).maxHeadersCount = 20;
+      (server as any).timeout = 5000;
+      (server as any).keepAliveTimeout = 5000;
+      server.listen(listeningPort, 'localhost', 20, () => {
+        console.log('listening on %s://localhost:%d', (ishttps ? 'wss' : 'ws'), listeningPort);
+      });
+    } else {
+      server = http.createServer();
+      server.keepAliveTimeout = 5000;
+      server.timeout = 5000;
+      server.maxConnections = 20;
+      server.maxHeadersCount = 20;
+      server.listen(listeningPort, 'localhost', 20, () => {
+        console.log('listening on %s://localhost:%d', (ishttps ? 'wss' : 'ws'), listeningPort);
+      });
+    }
+
+    const wss = new ws.Server({ server, maxPayload: 50, backlog: 20 });
+
+    this.initHeartbeat(wss);
+
+    wss.on('connection', (websocket: ws, req: http.IncomingMessage) => {
+      (websocket as any).isAlive = true;
+      let address = req.socket.remoteAddress;
+      let port = req.socket.remotePort;
+      let ip = req.headers['x-forwarded-for'];
+      if (ip && ip.length > 0) {
+        ip = ip.slice(0, ip.indexOf(','));
+        address = ip.slice(0, ip.indexOf(':')).toString();
+        port = Number.parseInt(ip.slice(ip.indexOf(':')).toString(), 10);
+      }
+      console.log('[%s] [%s:%s] connected', new Date().toISOString(), address, port);
+
+      websocket.on('open', () => {
+        const timestamp = new Date().toISOString();
+        console.log('[%s] [%s:%s] opened', timestamp, address, port);
+      });
+
+      websocket.on('message', (message) => {
+        let timestamp = new Date().toISOString();
+        console.log('[%s] [%s:%s] receiving', timestamp, address, port);
+        let len = 0;
+        let text = '';
+        if (typeof message === 'string') {
+          len = message.length;
+          text = 'chars string';
         } else {
-            server = http.createServer();
-            server.keepAliveTimeout = 5000;
-            server.timeout = 5000;
-            server.maxConnections = 20;
-            server.maxHeadersCount = 20;
-            server.listen(listeningPort, "localhost", 20, () => {
-                console.log('listening on %s://localhost:%d', (ishttps ? 'wss' : 'ws'), listeningPort);
-            });
+          len = (message as ArrayBuffer).byteLength;
+          text = 'bytes';
         }
+        timestamp = new Date().toISOString();
+        console.log('[%s] [%s:%s] received %d %s', timestamp, address, port, len, text);
+        websocket.send(message);
+        timestamp = new Date().toISOString();
+        console.log('[%s] [%s:%s] sent %d %s', timestamp, address, port, len, text);
+      });
 
-        const wss = new ws.Server({ server: server, maxPayload: 50, backlog: 20 });
+      websocket.on('close', (code, message) => {
+        const timestamp = new Date().toISOString();
+        console.log('[%s] [%s:%s] closed %s (%d)', timestamp, address, port, message, code);
+      });
 
-        this.initHeartbeat(wss);
+      websocket.on('error', (err) => {
+        const timestamp = new Date().toISOString();
+        console.log('[%s] [%s:%s] error %s: %s: %s', timestamp, address, port, err.name, err.message, err.stack);
+      });
 
-        wss.on('connection', (websocket: ws, req: http.IncomingMessage) => {
-            (<any>websocket).isAlive = true;
-            let address = req.socket.remoteAddress;
-            let port = req.socket.remotePort;
-            let ip = req.headers['x-forwarded-for'];
-            if (ip && ip.length > 0) {
-                ip = ip.slice(0, ip.indexOf(','))
-                address = ip.slice(0, ip.indexOf(':')).toString();
-                port = Number.parseInt(ip.slice(ip.indexOf(':')).toString());
-            }
-            let timestamp = new Date().toISOString();
-            console.log('[%s] [%s:%s] connected', timestamp, address, port);
+      websocket.on('pong', (data) => {
+        (websocket as any).isAlive = true;
+      });
+    });
 
-            websocket.on('open', () => {
-                let timestamp = new Date().toISOString();
-                console.log('[%s] [%s:%s] opened', timestamp, address, port);
-            });
+    wss.on('error', (err) => {
+      const timestamp = new Date().toISOString();
+      console.log('[%s] error %s: %s: %s', timestamp, err.name, err.message, err.stack);
+    });
 
-            websocket.on('message', (message) => {
-                let timestamp = new Date().toISOString();
-                console.log('[%s] [%s:%s] receiving', timestamp, address, port);
-                let len = 0;
-                let text = '';
-                if (typeof message === 'string') {
-                    len = message.length;
-                    text = 'chars string';
-                } else {
-                    len = (<ArrayBuffer>message).byteLength;
-                    text = 'bytes';
-                }
-                timestamp = new Date().toISOString();
-                console.log('[%s] [%s:%s] received %d %s', timestamp, address, port, len, text);
-                websocket.send(message);
-                timestamp = new Date().toISOString();
-                console.log('[%s] [%s:%s] sent %d %s', timestamp, address, port, len, text);
-            });
+    return wss;
+  }
 
-            websocket.on('close', (code, message) => {
-                let timestamp = new Date().toISOString();
-                console.log('[%s] [%s:%s] closed %s (%d)', timestamp, address, port, message, code);
-            });
-
-            websocket.on('error', (err) => {
-                let timestamp = new Date().toISOString();
-                console.log('[%s] [%s:%s] error %s: %s: %s', timestamp, address, port, err.name, err.message, err.stack);
-            });
-
-            websocket.on('pong', (data) => {
-                (<any>websocket).isAlive = true;
-            });
+  private initHeartbeat(wsserver: ws.Server) {
+    setInterval(() => {
+      wsserver.clients.forEach((wss) => {
+        if ((wss as any).isAlive === false) {
+          const timestamp = new Date().toISOString();
+          console.log('[%s] heartbeat terminate', timestamp);
+          return wss.terminate();
+        }
+        (wss as any).isAlive = false;
+        wss.ping('', false, (err: Error) => {
+          const timestamp = new Date().toISOString();
+          console.log('[%s] error %s: %s: %s', timestamp, err.name, err.message, err.stack);
         });
-
-        wss.on('error', (err) => {
-            let timestamp = new Date().toISOString();
-            console.log('[%s] error %s: %s: %s', timestamp, err.name, err.message, err.stack);
-        });
-
-        return wss;
-    }
-
-    private initHeartbeat(wsserver: ws.Server) {
-        setInterval(() => {
-            wsserver.clients.forEach((ws) => {
-                var x: any = ws;
-                if ((<any>ws).isAlive === false) {
-                    let timestamp = new Date().toISOString();
-                    console.log('[%s] heartbeat terminate', timestamp);
-                    return ws.terminate();
-                };
-                (<any>ws).isAlive = false;
-                ws.ping('', false, true);
-                //log(LOGTYPE.DEBUG, 'ping');
-            });
-        }, this.heartbeatInterval);
-    }
+        // log(LOGTYPE.DEBUG, 'ping');
+      });
+    }, this.heartbeatInterval);
+  }
 }
