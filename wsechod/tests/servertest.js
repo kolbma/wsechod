ab018eab009bb1a19c35fca56aa49f5c78a2031e
Markus Kolb <github@tower-net.de>
2019-04-05 01:39:51 +0200

    fixed mocha bdd test

--- wsechod/tests/servertest.js
+++ wsechod/tests/servertest.js
@@ -1,58 +1,51 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const assert = require("assert");
-const ws = require("ws");
-const server_1 = require("../server");
-describe('Test Suite Server', () => {
-    let s;
-    let ss;
-    let clients;
-    setup(() => {
+var assert = require("assert");
+var ws = require("ws");
+var server_1 = require("../server");
+describe('Test Suite Server', function () {
+    var s;
+    var ss;
+    var clients;
+    before(function (done) {
         console.log('setup');
         s = new server_1.Server().initServer(8080, false);
         ss = new server_1.Server().initServer(8443, true);
         clients = new Array();
         clients.push(new ws('ws://localhost:8080', {}));
         clients.push(new ws('wss://localhost:8443', { rejectUnauthorized: false }));
-    });
-    teardown(() => {
-        console.log('teardown');
-        s.close();
-        ss.close();
-    });
-    before((done) => {
-        console.log('before');
         done();
     });
-    after((done) => {
-        let c = 0;
-        clients.forEach((client, i, array) => {
-            console.log('after');
+    after(function (done) {
+        for (var _i = 0, clients_1 = clients; _i < clients_1.length; _i++) {
+            var client = clients_1[_i];
             client.close(1000);
-            c++;
-            if (c === clients.length) {
-                console.log('after');
-                done();
-            }
-        });
+        }
+        setTimeout(function () {
+            s.close();
+            ss.close();
+            done();
+        }, 500);
     });
-    it('Test echo', (done) => {
-        let c = 0;
+    it('Test echo', function (done) {
+        var c = 0;
         try {
-            clients.forEach((client, i, array) => {
-                client.on('open', () => {
-                    client.on('error', (err) => {
+            clients.forEach(function (client, i, array) {
+                client.on('open', function () {
+                    client.on('error', function (err) {
                         console.error(err);
                         assert.fail(err.message);
                         done(err);
                     });
-                    client.on('close', (code, message) => {
-                        console.log(code + ': ' + message);
-                        assert.ok(code === 1000);
-                        done(new Error('closed'));
+                    client.on('close', function (code, message) {
+                        // console.log(code + ': ' + message);
+                        if (code !== 1000) {
+                            assert.ok(code === 1000);
+                            done(new Error('closed'));
+                        }
                     });
-                    client.on('ping', (data) => { client.pong(); });
-                    client.on('message', (message) => {
+                    client.on('ping', function (data) { client.pong(); });
+                    client.on('message', function (message) {
                         if (message === 'hello world') {
                             c++;
                             console.log('message ok');
@@ -67,7 +60,7 @@ describe('Test Suite Server', () => {
                             done(new Error('message not expected: ' + message));
                         }
                     });
-                    client.send('hello world', (err) => {
+                    client.send('hello world', function (err) {
                         if (err) {
                             console.error(err);
                             assert.fail(err.message);
--- wsechod/tests/servertest.ts
+++ wsechod/tests/servertest.ts
@@ -5,87 +5,77 @@ import { Server } from '../server';
 
 describe('Test Suite Server', () => {
 
-    let s: ws.Server;
-    let ss: ws.Server;
-    let clients: ws[];
+  let s: ws.Server;
+  let ss: ws.Server;
+  let clients: ws[];
 
-    setup(() => {
-        console.log('setup');
-        s = new Server().initServer(8080, false);
-        ss = new Server().initServer(8443, true);
-        clients = new Array<ws>();
-        clients.push(new ws('ws://localhost:8080', {}));
-        clients.push(new ws('wss://localhost:8443', { rejectUnauthorized: false }));
-    });
+  before((done) => {
+    console.log('setup');
+    s = new Server().initServer(8080, false);
+    ss = new Server().initServer(8443, true);
+    clients = new Array<ws>();
+    clients.push(new ws('ws://localhost:8080', {}));
+    clients.push(new ws('wss://localhost:8443', { rejectUnauthorized: false }));
+    done();
+  });
 
-    teardown(() => {
-        console.log('teardown');
-        s.close();
-        ss.close();
-    })
+  after((done) => {
+    for (const client of clients) {
+      client.close(1000);
+    }
+    setTimeout(() => {
+      s.close();
+      ss.close();
+      done();
+    }, 500);
+  });
 
-    before((done) => {
-        console.log('before');
-        done();
-    });
-
-    after((done) => {
-        let c = 0;
-        clients.forEach((client, i, array) => {
-            console.log('after');
-            client.close(1000);
-            c++;
-            if (c === clients.length) {
-                console.log('after');
-                done();
-            }
-        });
-    });
-
-    it('Test echo', (done) => {
-        let c = 0;
-        try {
-            clients.forEach((client, i, array) => {
-                client.on('open', () => {
-                    client.on('error', (err) => {
-                        console.error(err);
-                        assert.fail(err.message);
-                        done(err);
-                    });
-                    client.on('close', (code, message) => {
-                        console.log(code + ': ' + message);
-                        assert.ok(code === 1000);
-                        done(new Error('closed'));
-                    });
-                    client.on('ping', (data) => { client.pong(); });
-                    client.on('message', (message) => {
-                        if (message === 'hello world') {
-                            c++;
-                            console.log('message ok');
-                            if (c === clients.length) {
-                                assert.ok('finished');
-                                done();
-                            }
-                        } else {
-                            console.log('message wrong');
-                            assert.notStrictEqual(message, 'hello world');
-                            done(new Error('message not expected: ' + message));
-                        }
-                    });
-
-                    client.send('hello world', (err) => {
-                        if (err) {
-                            console.error(err);
-                            assert.fail(err.message);
-                            done(err);
-                        }
-                        console.log('sent');
-                    });
-                });
-            });
-        } catch (err) {
+  it('Test echo', (done) => {
+    let c = 0;
+    try {
+      clients.forEach((client, i, array) => {
+        client.on('open', () => {
+          client.on('error', (err) => {
             console.error(err);
-        }
-    }).timeout(5000);
+            assert.fail(err.message);
+            done(err);
+          });
+          client.on('close', (code, message) => {
+            // console.log(code + ': ' + message);
+            if (code !== 1000) {
+              assert.ok(code === 1000);
+              done(new Error('closed'));
+            }
+          });
+          client.on('ping', (data) => { client.pong(); });
+          client.on('message', (message) => {
+            if (message === 'hello world') {
+              c++;
+              console.log('message ok');
+              if (c === clients.length) {
+                assert.ok('finished');
+                done();
+              }
+            } else {
+              console.log('message wrong');
+              assert.notStrictEqual(message, 'hello world');
+              done(new Error('message not expected: ' + message));
+            }
+          });
+
+          client.send('hello world', (err) => {
+            if (err) {
+              console.error(err);
+              assert.fail(err.message);
+              done(err);
+            }
+            console.log('sent');
+          });
+        });
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }).timeout(5000);
 
 });
